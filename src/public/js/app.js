05ad1bcbacfbd4f08ce5cfe17485732b189179fe
김동구 <bbonggu92@gmail.com>
2021-10-03 13:29:25 +0900

    Add Socket Connection between Back and Front

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,10 @@
-hello;
+const url = `ws://${window.location.host}`;
+const socket = new WebSocket(url);
+
+socket.addEventListener('open', () => console.log('Connected to Server!'));
+socket.addEventListener('message', (message) => console.log(message.data));
+socket.addEventListener('close', () => console.log('Disconnected from Server!'));
+
+setTimeout(() => {
+  socket.send('hello from Client :)');
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -3,23 +3,22 @@ import ws from 'ws';
 import express from 'express';
 
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', `${__dirname}/views`);
 
 app.use('/public', express.static(`${__dirname}/public`));
-app.get('/', (_, res) => {
-  res.render('home');
-});
-
-app.get('/*', (_, res) => {
-  res.redirect('/');
-});
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const server = http.createServer(app);
 const wss = new ws.Server({ server });
 
-const onListen = () => {
-  console.log('Server is running on Port 3000');
-};
+wss.on("connection", (socket) => {
+  console.log('Connected to Client!');
+  socket.on('close', () => console.log('Disconnected from Client!'));
+  socket.on('message', (message) => console.log('message : ' + message));
+  socket.send('welcome to Chat server :)');
+});
 
-server.listen(3000, onListen);
+server.listen(3000, () => console.log('Server is running on Port 3000'));
