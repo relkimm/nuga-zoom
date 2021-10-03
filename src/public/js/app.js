560eaa824ba73aaedde3ba33ecf21b1df1e99df3
김동구 <bbonggu92@gmail.com>
2021-10-03 22:18:51 +0900

    Add seperate chat & nickname messsage by type

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,22 +2,51 @@ const url = `ws://${window.location.host}`;
 const socket = new WebSocket(url);
 
 socket.addEventListener('open', () => console.log('Connected to Server!'));
-socket.addEventListener('message', message => {
-  const liElement = document.createElement('li');
-  liElement.innerText = message.data;
-  const chatList = document.querySelector('#chat-list');
-  chatList.appendChild(liElement);
-});
 socket.addEventListener('close', () => console.log('Disconnected from Server!'));
 
-
+socket.addEventListener('message', message => {
+  const chatItem = getChatItem(parseMessage(message));
+  if(chatItem === undefined) {
+    return;
+  }
+  appendChatItem(chatItem);
+});
 
 const onSubmit = (event) => {
   event.preventDefault();
   const input = document.querySelector('#chat-input');
-  socket.send(input.value);
+  socket.send(makeMessage('chat', input.value));
   input.value = '';
+}
+
+const onNickSubmit = (event) => {
+  event.preventDefault();
+  const input = document.querySelector('#nick-input');
+  socket.send(makeMessage('nickname', input.value));
 }
 
 const chatForm = document.querySelector('#chat-form');
 chatForm.addEventListener('submit', onSubmit);
+const nickForm = document.querySelector('#nick-form');
+nickForm.addEventListener('submit', onNickSubmit);
+
+function makeMessage(type, payload) {
+  const message = { type, payload };
+  return JSON.stringify(message);
+}
+
+function parseMessage(message) {
+  return JSON.parse(message.data);
+}
+
+function getChatItem(parsedMessage) {
+  return parsedMessage.type === 'chat' ? parsedMessage.payload : undefined;
+}
+
+function appendChatItem(chatItem) {
+  const liElement = document.createElement('li');
+  liElement.innerText = chatItem;
+
+  const chatList = document.querySelector('#chat-list');
+  chatList.append(liElement);
+}
