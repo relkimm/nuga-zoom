5c6a306441fd1e2b04315460b464d35bbe5a59c8
김동구 <bbonggu92@gmail.com>
2021-10-04 22:13:17 +0900

    Update WebSocket to SocketIO

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,52 +1,74 @@
-const url = `ws://${window.location.host}`;
-const socket = new WebSocket(url);
+const socket = io();
 
-socket.addEventListener('open', () => console.log('Connected to Server!'));
-socket.addEventListener('close', () => console.log('Disconnected from Server!'));
-
-socket.addEventListener('message', message => {
-  const chatItem = getChatItem(parseMessage(message));
-  if(chatItem === undefined) {
-    return;
-  }
-  appendChatItem(chatItem);
-});
+const room = document.querySelector('#room');
+const roomForm = room.querySelector('form');
 
 const onSubmit = (event) => {
   event.preventDefault();
-  const input = document.querySelector('#chat-input');
-  socket.send(makeMessage('chat', input.value));
+  const input = roomForm.querySelector('input');
+  /* send */
+  socket.emit('enter_room', { payload: input.value }, () => {
+    console.log('server is done');
+  });
+  /* clear */
   input.value = '';
 }
 
-const onNickSubmit = (event) => {
-  event.preventDefault();
-  const input = document.querySelector('#nick-input');
-  socket.send(makeMessage('nickname', input.value));
-}
 
-const chatForm = document.querySelector('#chat-form');
-chatForm.addEventListener('submit', onSubmit);
-const nickForm = document.querySelector('#nick-form');
-nickForm.addEventListener('submit', onNickSubmit);
 
-function makeMessage(type, payload) {
-  const message = { type, payload };
-  return JSON.stringify(message);
-}
+roomForm.addEventListener('submit', onSubmit);
 
-function parseMessage(message) {
-  return JSON.parse(message.data);
-}
 
-function getChatItem(parsedMessage) {
-  return parsedMessage.type === 'chat' ? `${parsedMessage.nickname} : ${parsedMessage.payload}` : undefined;
-}
 
-function appendChatItem(chatItem) {
-  const liElement = document.createElement('li');
-  liElement.innerText = chatItem;
+// const url = `ws://${window.location.host}`;
+// const socket = new WebSocket(url);
 
-  const chatList = document.querySelector('#chat-list');
-  chatList.append(liElement);
-}
+// socket.addEventListener('open', () => console.log('Connected to Server!'));
+// socket.addEventListener('close', () => console.log('Disconnected from Server!'));
+
+// socket.addEventListener('message', message => {
+//   const chatItem = getChatItem(parseMessage(message));
+//   if(chatItem === undefined) {
+//     return;
+//   }
+//   appendChatItem(chatItem);
+// });
+
+// const onSubmit = (event) => {
+//   event.preventDefault();
+//   const input = document.querySelector('#chat-input');
+//   socket.send(makeMessage('chat', input.value));
+//   input.value = '';
+// }
+
+// const onNickSubmit = (event) => {
+//   event.preventDefault();
+//   const input = document.querySelector('#nick-input');
+//   socket.send(makeMessage('nickname', input.value));
+// }
+
+// const chatForm = document.querySelector('#chat-form');
+// chatForm.addEventListener('submit', onSubmit);
+// const nickForm = document.querySelector('#nick-form');
+// nickForm.addEventListener('submit', onNickSubmit);
+
+// function makeMessage(type, payload) {
+//   const message = { type, payload };
+//   return JSON.stringify(message);
+// }
+
+// function parseMessage(message) {
+//   return JSON.parse(message.data);
+// }
+
+// function getChatItem(parsedMessage) {
+//   return parsedMessage.type === 'chat' ? `${parsedMessage.nickname} : ${parsedMessage.payload}` : undefined;
+// }
+
+// function appendChatItem(chatItem) {
+//   const liElement = document.createElement('li');
+//   liElement.innerText = chatItem;
+
+//   const chatList = document.querySelector('#chat-list');
+//   chatList.append(liElement);
+// }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import http from 'http';
-import ws from 'ws';
+// import ws from 'ws';
 import express from 'express';
-import socketStore from './stores/socketStore';
-import { ChatMessage } from './models/ChatMessage';
-import { v4 } from 'uuid';
+// import socketStore from './stores/socketStore';
+// import { ChatMessage } from './models/ChatMessage';
+// import { v4 } from 'uuid';
+import SocketIO from 'socket.io';
 
 const app = express();
 
@@ -15,42 +16,54 @@ app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
 const server = http.createServer(app);
-const wss = new ws.Server({ server });
+const io = SocketIO(server);
 
-wss.on("connection", (socket) => {
-  socket['nickname'] = 'anonymous';
-  socket['id'] = v4();
-  socketStore.save(socket);
-
-  console.log('Connected to Client!');
-  
-  socket.on('close', () => console.log('Disconnected from Client!'));
-
-  socket.on('message', (message) => {
-    const parsedMessage = parseMessage(message);
-    const targetNickname = socket.nickname;
-
-    switch(parsedMessage.type) {
-      case 'chat': {
-        const sockets = socketStore.find();
-        sockets.forEach((s) => {
-          const chatMessage = new ChatMessage(
-            targetNickname, 
-            parsedMessage.type, 
-            parsedMessage.payload
-          );
-          s.send(JSON.stringify(chatMessage));
-        });
-        break;
-      }
-      case 'nickname': {
-        socket['nickname'] = parsedMessage.payload;
-        socketStore.update(socket);
-        break;
-      }
-    }
+io.on('connection', socket => {
+  socket.on('enter_room', (message, done) => {
+    console.log('message : ', message);
+    setTimeout(() => {
+      done();
+    }, 1000);
   });
 });
+
+
+// const wss = new ws.Server({ server });
+
+// wss.on("connection", (socket) => {
+//   socket['nickname'] = 'anonymous';
+//   socket['id'] = v4();
+//   socketStore.save(socket);
+
+//   console.log('Connected to Client!');
+  
+//   socket.on('close', () => console.log('Disconnected from Client!'));
+
+//   socket.on('message', (message) => {
+//     const parsedMessage = parseMessage(message);
+//     const targetNickname = socket.nickname;
+
+//     switch(parsedMessage.type) {
+//       case 'chat': {
+//         const sockets = socketStore.find();
+//         sockets.forEach((s) => {
+//           const chatMessage = new ChatMessage(
+//             targetNickname, 
+//             parsedMessage.type, 
+//             parsedMessage.payload
+//           );
+//           s.send(JSON.stringify(chatMessage));
+//         });
+//         break;
+//       }
+//       case 'nickname': {
+//         socket['nickname'] = parsedMessage.payload;
+//         socketStore.update(socket);
+//         break;
+//       }
+//     }
+//   });
+// });
 
 
 server.listen(3000, () => console.log('Server is running on Port 3000'));
