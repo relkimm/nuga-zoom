5fc94c4ec4ff7304f9d12a2c7c7679da0289049a
김동구 <bbonggu92@gmail.com>
2021-10-10 20:28:56 +0900

    Add cors config

--- src/config/index.js
+++ src/config/index.js
@@ -15,3 +15,6 @@ export const DB_HOST = process.env.DB_HOST;
 export const DB_PORT = process.env.DB_PORT;
 export const DB_USERNAME = process.env.DB_USERNAME;
 export const DB_PASSPORT = process.env.DB_PASSPORT;
+
+/* View */
+export const VIEW_ENGINE = process.env.VIEW_ENGINE;
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import logger from 'morgan';
-import { ROOT_PATH } from '../config';
-import { LOG_LEVEL } from '../config';
+import { ROOT_PATH, LOG_LEVEL, VIEW_ENGINE } from '../config';
 import mainRouter from '../routes';
 
 
 export default async (app) => {
-  app.set('view engine', 'pug');
+  app.set('view engine', VIEW_ENGINE);
   app.set('views', path.join(ROOT_PATH, 'src', 'views'));
+  app.use(cors({
+    origin: 'localhost',
+    credentials: true,
+  }));
   app.use(logger(LOG_LEVEL));
   app.use('/public', express.static(path.join(ROOT_PATH, 'src', 'public')));
   app.use(mainRouter);
