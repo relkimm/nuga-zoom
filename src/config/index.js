0c9c5c60a9cca78236fb596101fdef9d6cc61bfd
김동구 <bbonggu92@gmail.com>
2021-10-11 13:59:33 +0900

    Add Cookie & Session Config

--- src/config/index.js
+++ src/config/index.js
@@ -10,6 +10,10 @@ export const SERVER_PORT = process.env.SERVER_PORT;
 /* Logger */
 export const LOG_LEVEL = process.env.LOG_LEVEL;
 
+/* Cookie & Session */
+export const COOKIE_KEY = process.env.COOKIE_KEY;
+export const SESSION_KEY = process.env.SESSION_KEY;
+
 /* DB */
 export const MONGO_URI = process.env.MONGO_URI;
 
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -2,20 +2,34 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import logger from 'morgan';
-import { ROOT_PATH, LOG_LEVEL, VIEW_ENGINE } from '../config';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import { ROOT_PATH, LOG_LEVEL, COOKIE_KEY, SESSION_KEY, VIEW_ENGINE } from '../config';
 import mainRouter from '../routes';
-
 
 export default async (app) => {
   app.set('view engine', VIEW_ENGINE);
   app.set('views', path.join(ROOT_PATH, 'src', 'views'));
-  app.use(cors({
-    origin: 'localhost',
-    credentials: true,
-  }));
+
+  app.use(cors({ origin: 'localhost', credentials: true }));
   app.use(logger(LOG_LEVEL));
+  
   app.use('/public', express.static(path.join(ROOT_PATH, 'src', 'public')));
+  
+  app.use(cookieParser(COOKIE_KEY));
+  app.use(session({
+    secret: SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      signed: true,
+    }
+  }));
+  
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  
   app.use(mainRouter);
   return app;
 }
